3153bcc2152cc27d1cf43cefad20d7f700495e85
aidanreynolds <aidanreynolds@gmail.com>
2020-10-19 10:03:31 -0400

    swagger2

--- src/main/java/com/spinozanose/springbootrestoo/SpringbootRestOoApplication.java
+++ src/main/java/com/spinozanose/springbootrestoo/SpringbootRestOoApplication.java
@@ -2,21 +2,25 @@ package com.spinozanose.springbootrestoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootRestOoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestOoApplication.class, args);
 	}
 
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.any())
-//				.paths(PathSelectors.regex("/spanish-greeting.*"))
-//				.build();
-//	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.spinozanose.springbootrestoo"))
+				.build();
+	}
 
 }
